f61efbac913c69783fe23597b3c393dcc7cba99a
Shadab Ali <shadabali604@gmail.com>
2020-10-30 23:50:09 +0530

    added:new features

--- js/main.js
+++ js/main.js
@@ -13,23 +13,19 @@ if(navigator.geolocation){
 
 
 })*/
-
 const rawCity = document.querySelector('#input');
 const search = document.querySelector('#submit');
-let city = rawCity.value;
-
-
 
 function CreateUI(jsonResponse){
-    c
-    const nameOfCity = `<h1>${jsonResponse.name}</h1>`;
+    const nameOfCity = `<h3>${jsonResponse.name}, ${jsonResponse.sys.country} Weather</h3>`;
     console.log(jsonResponse.main.temp.value);
-    const temp = `<p> Current Temp: ${jsonResponse.main.temp} </p>`;
+    const temp = `<h3'><span class = 'text-warning display-2'> ${jsonResponse.main.temp}</span><span>°C</span> </h3>`;
     const tempMax = `<p>  Temp-max: ${jsonResponse.main.temp_max}</p>`;
     const tempMin = `<p>  Temp-min: ${jsonResponse.main.temp_min}</p>`;
-    const des = `<p> Weather Description: ${jsonResponse.weather[0].description}</p>`;
-    const wind = `<p> Weather Wind Speed: ${jsonResponse.wind.speed} </p>`;
-    const windD = `<p> Weather Wind Speed Degree: ${jsonResponse.wind.deg}</p>`;
+    const des = `<p class = 'text-muted'>${jsonResponse.weather[0].description}</p>`;
+    const wind = `<p>${jsonResponse.wind.speed} </p>`;
+    const windD = `<p>${jsonResponse.wind.deg}</p>`;
+    const currentTime = `<p class = 'text-muted'> last updated on ${new Date().toLocaleString()}</p>`
     render(nameOfCity, '#cityName');
     render(temp, '#temp');
     render(tempMax, '#temp-max');
@@ -37,31 +33,33 @@ function CreateUI(jsonResponse){
     render(des, '#des');
     render(wind, '#wind');
     render(windD, "#windD");
+    render(currentTime, "#current-time");
 }
 
 search.addEventListener('click', (e) => {
  e.preventDefault();
-    getData(city)
+ let city = rawCity.value;
+     getData(city)
 
 });
 
 rawCity.addEventListener('keypress', (e) => {
  if(e == 'Enter'){
+    let city = rawCity.value;
      getData(city)
  }
 })
-const getData = async (city = 'Delhi') => {
+const getData = async (city = 'delhi') => {
     const apiKey = "8e4bcc04c2bc500c41a818e2f18046e5";
      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},india&APPID=${apiKey}&units=metric`;
     const response = await fetch(url);
     const jsonResponse = await response.json();
     console.log(jsonResponse);
-    CreateUI(jsonResponse)
+    CreateUI(jsonResponse);
 }
 
 
 getData();
-
 var render = function (template, selector) {
     var node = document.querySelector(selector);
     if (!node) return;
